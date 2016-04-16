85666ae940b0ebd08749cf114a6377b2a3344e52
qsis2140 <SHQ7@pitt.edu>
2016-04-15 21:29:24 -0400

    src

--- Classes/Path.java
+++ Classes/Path.java
@@ -1,11 +1,13 @@
 package Classes;
 
 public class Path {
-	public static String DataWebDir="data//docset.trecweb";//address of docset.trectext
+	//public static String DataWebDir="data//docset.trecweb";//address of docset.trectext
 	public static String DataTextDir="data//docset.trectext";//address of docset.trectext
 	
 	public static String ResultHM1="data//result.";//
 	
 	public static String StopwordDir="data//stopword.txt";//address of stopwords.txt
+	public static String DataDir="data//pmc-text-00/00//2630847.nxml"; //data
+
 
 }
--- PreProcessData/TrectextCollection.java
+++ PreProcessData/TrectextCollection.java
@@ -1,9 +1,14 @@
 package PreProcessData;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
 import Classes.Path;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -22,26 +27,75 @@ public class TrectextCollection implements DocumentCollection {
         private BufferedReader reader = null; 
         private String line = null;
         private Map<String, Object> doc = new HashMap<String,Object>();
-        
+        private List<File> filelist;
+        private int filenum = 0;
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public TrectextCollection() throws IOException {
 		// This constructor should open the file in Path.DataTextDir
 		// and also should make preparation for function nextDocument()
                 // Do not load the whole corpus into memory!!!
 	
-            // open and read the trectext file
-            fis = new FileInputStream(Path.DataTextDir);
+
+			
+			//获取文件列表
+			System.out.println("acquiring file list");
+			filelist = new ArrayList<File>();
+			filelist = getFileList(Path.DataTextDir);
+			
+            fis = new FileInputStream(filelist.get(filenum).getAbsoluteFile());
             reader = new BufferedReader(new InputStreamReader(fis));
-            
+            System.out.println("reading files");
             
 	}
 	
+	//获取文件的函数
+	public List<File> getFileList(String strPath) {
+        File dir = new File(strPath);
+        File[] files = dir.listFiles(); // 该文件目录下文件全部放入数组
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                String fileName = files[i].getName();
+                if (files[i].isDirectory()) { // 判断是文件还是文件夹
+                    getFileList(files[i].getAbsolutePath()); // 获取文件绝对路径
+                } 
+                else  { 
+                    String strFileName = files[i].getAbsolutePath();
+                   // System.out.println("---" + strFileName);
+                    filelist.add(files[i]);
+                } 
+            }
+
+        }
+        return filelist;
+    }
 	// YOU SHOULD IMPLEMENT THIS METHOD
 	public Map<String, Object> nextDocument() throws IOException {
-		// this method should load one document from the corpus, and return this document's number and content.
-		// the returned document should never be returned again.
-		// when no document left, return null
-		// NTT: remember to close the file that you opened, when you do not use it any more
+		//文档格式分析的部分没有写
+		int idindex;
+		String docNo = "";
+		String text = new String();
+		//doc.put(docNo, text.toCharArray());
+		
+		filenum++;
+		fis.close();
+		reader.close();
+		fis = new FileInputStream(filelist.get(filenum).getAbsoluteFile());
+        reader = new BufferedReader(new InputStreamReader(fis));
+        line = reader.readLine();
+        while(line != null) {
+        	if(line.indexOf("article-id pub-id-type=\"pmid\"")!= -1){
+        		idindex = line.indexOf("article-id pub-id-type=\"pmid\"") + 30;
+        		text = line.substring(idindex, idindex + 9);
+        		System.out.println(text);
+        	}
+        	else
+        		System.out.println(filelist.get(filenum).getAbsoluteFile());
+        	line = reader.readLine();
+        }
+        doc.put(docNo, text.toCharArray());
+		return doc;
+		/*
+		
             Pattern pDocBegin = Pattern.compile("<DOC>");
             Pattern pDocEnd = Pattern.compile("</DOC>");
             // match file withe patttern to get docno and content
@@ -83,7 +137,7 @@ public class TrectextCollection implements DocumentCollection {
             } else {
                 return doc;
             }
-            
+          */  
             
 	}   
 	
