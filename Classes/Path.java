6b13c83990a0edf9a4a3838a45e5eec84a6db392
spssspss0712 <spssspss0712@hotmail.com>
2016-04-20 15:37:14 -0400

    change path

--- Classes/Path.java
+++ Classes/Path.java
@@ -4,10 +4,10 @@ public class Path {
 	//public static String DataWebDir="data//docset.trecweb";//address of docset.trectext
 	public static String DataTextDir="data//docset.trectext";//address of docset.trectext
 	
-	public static String ResultHM1="data//result.";//
+	public static String ResultHM1="result.";//
 	
-	public static String StopwordDir="data//stopword.txt";//address of stopwords.txt
-	public static String DataDir="data//pmc-text-00/00//2630847.nxml"; //data
+	public static String StopwordDir="stopword.txt";//address of stopwords.txt
+	public static String DataDir="..//data"; //data
 
 
 }
